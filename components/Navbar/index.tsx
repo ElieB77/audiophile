3ea9b6e195099b7169113b3cdaa8e56b95ee3fef
Elie Bissor <eliebiss.26@gmail.com>
2022-11-11 12:53:13 +0100

    add Navbar cmp

---
+++ components/Navbar/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  const navbarLinks = [
+    { text: "home", href: "/" },
+    { text: "headphones", href: "/headphones" },
+    { text: "speakers", href: "/speakers" },
+    { text: "earphones", href: "/earphones" },
+  ];
+  return (
+    <div className={styles.__navbar}>
+      <Link href="/">
+        <Image src="/logo-audiophile.svg" alt="Logo" width={143} height={25} />
+      </Link>
+      <div className={styles.__links}>
+        {navbarLinks.map((link: any, index: number) => {
+          return (
+            <Link href={link.href} key={index}>
+              {link.text}
+            </Link>
+          );
+        })}
+      </div>
+      <Image src="/cart.svg" alt="Cart" width={23} height={20} />
+    </div>
+  );
+};
+
+export default Navbar;
