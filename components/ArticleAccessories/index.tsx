b91e762334fb433fcc5353504c0ff11f8b7e6d30
Elie Bissor <eliebiss.26@gmail.com>
2022-11-15 09:57:25 +0100

    add articleAccessory cmp

---
+++ components/ArticleAccessories/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.scss";
+
+interface Props {
+  content?: string;
+  //   accessories?: {
+  //     quantity: number;
+  //     accessory: string;
+  //   }[];
+  accessories: any;
+}
+
+const ArticleAccessories = ({ content, accessories }: Props) => {
+  return (
+    <div className={styles.__article_accessories}>
+      <div className={styles.__left_block}>
+        <h3>features</h3>
+        <p>{content}</p>
+      </div>
+      <div className={styles.__right_block}>
+        <h3>in the box</h3>
+        {accessories?.map((item: any, index: number) => {
+          return (
+            <div key={index}>
+              <span>{item.quantity}x</span>
+              <p>{item.accessory}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ArticleAccessories;
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import ArticleAccessories from "../../components/ArticleAccessories";
 import ArticleInfo from "../../components/ArticleInfo";
 
 const headphonesData = [
@@ -10,9 +11,26 @@ const headphonesData = [
     product_image:
       "/product-zx9-speaker/desktop/image-category-page-preview.jpg",
     price: 1750,
+    product_content: `These headphones have been created from durable, high-quality materials tough enough to take anywhere. Its compact folding design fuses comfort and minimalist style making it perfect for travel. Flawless transmission is assured by the latest wireless technology engineered for audio synchronization with videos.
+
+    More than a simple pair of headphones, this headset features a pair of built-in microphones for clear, hands-free calling when paired with a compatible smartphone. Controlling music and calls is also intuitive thanks to easy-access touch buttons on the earcups. Regardless of how you use the XX59 headphones, you can do so all day thanks to an impressive 30-hour battery life that can be rapidly recharged via USB-C.`,
+    product_accessories: [
+      {
+        accessory: "headphone unit",
+        quantity: 2,
+      },
+      {
+        accessory: "Replacement Earcups",
+        quantity: 3,
+      },
+      {
+        accessory: "user manual",
+        quantity: 1,
+      },
+    ],
   },
 ];
-
+console.log(headphonesData[0].product_accessories);
 const Product = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -24,6 +42,10 @@ const Product = () => {
         description={headphonesData[0].product_description}
         price={headphonesData[0].price}
         showCounterQuantity
+      />
+      <ArticleAccessories
+        content={headphonesData[0].product_content}
+        accessories={headphonesData[0].product_accessories}
       />
     </div>
   );
