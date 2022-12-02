3a9901db7290afb9ce815a812a8e1318a2860e29
Elie Bissor <eliebiss.26@gmail.com>
2022-12-02 18:09:07 +0100

    add ctx function

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,16 +1,24 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
 interface CartProviderProps {
   children: ReactNode;
 }
 
-interface CartItems {
+interface CartItem {
   id: number;
+  image: string;
+  name: string;
+  price: number;
   quantity: number;
 }
 
 interface CartContext {
-  increaseCartQuantity: (id: number) => void;
+  addToCart: (id: number, image: string, name: string, price: number) => void;
+  getItemQuantity: (id: number) => number;
+  cartQuantity: number;
+  cartItems: CartItem[];
+  increaseItemQuantity: any;
+  clearCart: () => void;
 }
 
 const CartContext = createContext({} as CartContext);
@@ -20,11 +28,48 @@ export const useCart = () => {
 };
 
 export const CartProvider = ({ children }: CartProviderProps) => {
-  const increaseCartQuantity = () => {
-    return console.log("increaseCartQuantity");
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart = (
+    id: number,
+    image: string,
+    name: string,
+    price: number
+  ) => {
+    const alreadyExist = cartItems.find((item) => item.id === id);
+    if (!alreadyExist) {
+      setCartItems([...cartItems, { id, image, name, price, quantity: 1 }]);
+    }
   };
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  };
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const increaseItemQuantity = () => {
+    return console.log("increase qty");
+  };
+
+  const clearCart = () => {
+    return setCartItems([]);
+  };
+
   return (
-    <CartContext.Provider value={{ increaseCartQuantity }}>
+    <CartContext.Provider
+      value={{
+        addToCart,
+        getItemQuantity,
+        cartQuantity,
+        cartItems,
+        increaseItemQuantity,
+        clearCart,
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
