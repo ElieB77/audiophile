f0be58c07c26f2384dd6324f2321755ec441f3a3
Elie Bissor <eliebiss.26@gmail.com>
2022-12-06 11:59:12 +0100

    change content of cart modal when empty

--- components/Cart/CartModal/index.tsx
+++ components/Cart/CartModal/index.tsx
@@ -8,6 +8,7 @@ import CartItem from "../CartItem";
 import Image from "next/image";
 // Context
 import { useCart } from "../../../context/CartContext";
+import Link from "next/link";
 
 interface CartModalProps {
   show: any;
@@ -21,31 +22,50 @@ const CartModal = ({ show, handleClick }: CartModalProps) => {
     <>
       <div className={styles.__overlay} onClick={handleClick}></div>
       <div className={styles.__modal}>
-        <div className={styles.__head}>
-          <h6>
-            CART <span>({cartQuantity})</span>
-          </h6>
-          <p onClick={() => clearCart()}>Remove All</p>
-        </div>
-        <div className={styles.__body}>
-          {cartItems.map((item: any, index: number) => {
-            return (
-              <CartItem
-                key={index}
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                id={item.id}
-              />
-            );
-          })}
-        </div>
-        <div className={styles.__price}>
-          <p>total</p>
-          <h6>{"$" + cartTotalPrice.toLocaleString()}</h6>
-          <Button btnContent="checkout" />
-        </div>
+        {cartQuantity !== 0 ? (
+          <>
+            <div className={styles.__head}>
+              <h6>
+                CART <span>({cartQuantity})</span>
+              </h6>
+              <p onClick={() => clearCart()}>Remove All</p>
+            </div>
+            <div className={styles.__body}>
+              {cartItems.map((item: any, index: number) => {
+                return (
+                  <CartItem
+                    key={index}
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    quantity={item.quantity}
+                    id={item.id}
+                  />
+                );
+              })}
+            </div>
+            <div className={styles.__price}>
+              <p>total</p>
+              <h6>{"$" + cartTotalPrice.toLocaleString()}</h6>
+              <Button btnContent="checkout" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.__empty_cart}>
+              <p>Your cart is empty !</p>
+              <p>
+                Before proceeding to checkout you must add some products to the
+                shopping cart. Check out our products !
+              </p>
+              <div className={styles.__links}>
+                <Link href="/category/headphones">Headphones</Link>
+                <Link href="/category/speakers">Speakers</Link>
+                <Link href="/category/earphones">Earphones</Link>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   ) : null;
