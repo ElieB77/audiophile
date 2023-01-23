6c8bdb780f0fa45195f2af3950c3ae10ae6991a6
Elie Bissor <eliebiss.26@gmail.com>
2023-01-23 17:31:41 +0100

    Completed success modal post order.

--- components/Popups/SuccessModal/index.tsx
+++ components/Popups/SuccessModal/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import Button from "../../UI/Button";
 import { useCart } from "../../../context/CartContext";
 import { useRouter } from "next/router";
+import CartItem from "../../Cart/CartItem";
+import { replaceString } from "../../../utilities/replaceString";
 
 interface SuccessModalProps {
   show?: any;
@@ -13,6 +15,11 @@ interface SuccessModalProps {
 const SuccessModal = ({ show, handleClick }: SuccessModalProps) => {
   const { cartItems, cartTotalPrice } = useCart();
   const router = useRouter();
+
+  const backToHomepage = () => {
+    router.push("/");
+  };
+
   return (
     show && (
       <>
@@ -22,13 +29,41 @@ const SuccessModal = ({ show, handleClick }: SuccessModalProps) => {
           <h3>thank you for your order</h3>
           <p>You will receive an email confirmation shortly.</p>
           <div className={styles.__order_summary}>
-            <div className={styles.__items}></div>
+            <div className={styles.__items}>
+              <>
+                <div className={styles.__first_item}>
+                  <div className={styles.__image}>
+                    <Image
+                      alt="Product"
+                      src={replaceString(cartItems[0].image, "public", "")}
+                      fill
+                      objectFit="cover"
+                    />
+                  </div>
+                  <div className={styles.__info}>
+                    <p>{cartItems[0].name}</p>
+                    <p>${cartItems[0].price.toLocaleString()}</p>
+                  </div>
+                  <p className={styles.__quantity}>x{cartItems[0].quantity}</p>
+                </div>
+              </>
+              {cartItems.length > 1 && (
+                <>
+                  <hr />
+                  <p>And {cartItems.length - 1} other item(s)</p>
+                </>
+              )}
+            </div>
             <div className={styles.__total}>
               <p>grand total</p>
               <h6>{"$" + cartTotalPrice.toLocaleString()}</h6>
             </div>
           </div>
-          <Button btnContent={"back to home"} isFullWidth />
+          <Button
+            onClick={backToHomepage}
+            btnContent={"back to home"}
+            isFullWidth
+          />
         </div>
       </>
     )
