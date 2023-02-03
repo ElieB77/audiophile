3920be01f85096751329732f00c29b2f8627bd7a
Elie Bissor <eliebiss.26@gmail.com>
2023-02-03 16:09:41 +0100

    Check cartItems length on order confirmation page.

--- pages/order-confirmation/index.tsx
+++ pages/order-confirmation/index.tsx
@@ -18,60 +18,62 @@ const OrderConfirmation = () => {
   };
 
   return (
-    <div className={styles.__order_confirmation}>
-      <div className={styles.__wrapper}>
-        {typeof window !== "undefined" && (
-          <Confetti
-            width={window.innerWidth}
-            height={window.innerHeight}
-            numberOfPieces={50}
-          />
-        )}
-        <Image src={IconCart} alt={"Success icon"} />
-        <h3>thank you for your order!</h3>
-        <p>You will receive an email confirmation shortly.</p>
-        <div className={styles.__order_summary}>
-          <div className={styles.__items}>
-            <>
-              <div className={styles.__first_item}>
-                <div className={styles.__image}>
-                  <Image
-                    alt="Product"
-                    src={`/static${replaceString(
-                      cartItems[0].image && cartItems[0].image,
-                      "public",
-                      ""
-                    )}`}
-                    fill
-                    objectFit="cover"
-                  />
-                </div>
-                <div className={styles.__info}>
-                  <p>{cartItems[0].name}</p>
-                  <p>${cartItems[0].price.toLocaleString()}</p>
-                </div>
-                <p className={styles.__quantity}>x{cartItems[0].quantity}</p>
-              </div>
-            </>
-            {cartItems.length > 1 && (
+    cartItems.length !== 0 && (
+      <div className={styles.__order_confirmation}>
+        <div className={styles.__wrapper}>
+          {typeof window !== "undefined" && (
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              numberOfPieces={50}
+            />
+          )}
+          <Image src={IconCart} alt={"Success icon"} />
+          <h3>thank you for your order!</h3>
+          <p>You will receive an email confirmation shortly.</p>
+          <div className={styles.__order_summary}>
+            <div className={styles.__items}>
               <>
-                <hr />
-                <p>And {cartItems.length - 1} other item(s)</p>
+                <div className={styles.__first_item}>
+                  <div className={styles.__image}>
+                    <Image
+                      alt="Product"
+                      src={`/static${replaceString(
+                        cartItems[0].image && cartItems[0].image,
+                        "public",
+                        ""
+                      )}`}
+                      fill
+                      objectFit="cover"
+                    />
+                  </div>
+                  <div className={styles.__info}>
+                    <p>{cartItems[0].name}</p>
+                    <p>${cartItems[0].price.toLocaleString()}</p>
+                  </div>
+                  <p className={styles.__quantity}>x{cartItems[0].quantity}</p>
+                </div>
               </>
-            )}
+              {cartItems.length > 1 && (
+                <>
+                  <hr />
+                  <p>And {cartItems.length - 1} other item(s)</p>
+                </>
+              )}
+            </div>
+            <div className={styles.__total}>
+              <p>grand total</p>
+              <h6>{"$" + cartTotalPrice.toLocaleString()}</h6>
+            </div>
           </div>
-          <div className={styles.__total}>
-            <p>grand total</p>
-            <h6>{"$" + cartTotalPrice.toLocaleString()}</h6>
-          </div>
+          <Button
+            onClick={backToHomepage}
+            btnContent={"back to home"}
+            isFullWidth
+          />
         </div>
-        <Button
-          onClick={backToHomepage}
-          btnContent={"back to home"}
-          isFullWidth
-        />
       </div>
-    </div>
+    )
   );
 };
 
